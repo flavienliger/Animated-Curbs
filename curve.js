5bb5c454d05c73634fdd9b5572d1bf96063afb21
lieo <logibic@oila.fr>
2014-07-30 23:47:16 +0200

    add requestanimframe

--- curve.js
+++ curve.js
@@ -6,12 +6,14 @@ function CurveAnimator(from,to,c1,c2){
 	this.updatePath();
 	CurveAnimator.lastCreated = this;
 }
-CurveAnimator.prototype.animate = function(duration,callback,delay){
+
+CurveAnimator.prototype.animate = function(duration,callback){
 	var curveAnim = this;
-	// TODO: Use requestAnimationFrame if a delay isn't passed
-	if (!delay) delay = 1/40;
+		
 	clearInterval(curveAnim.animTimer);
+	
 	var startTime = new Date;
+	
 	curveAnim.animTimer = setInterval(function(){
 		var now = new Date;
 		var elapsed = (now-startTime)/1000;
@@ -23,38 +25,57 @@ CurveAnimator.prototype.animate = function(duration,callback,delay){
 		var p1 = curveAnim.pointAt(percent-0.01),
 			p2 = curveAnim.pointAt(percent+0.01);
 		callback(curveAnim.pointAt(percent),Math.atan2(p2.y-p1.y,p2.x-p1.x)*180/Math.PI);
-	},delay*1000);
+	},1/40*1000);
 };
+
+
+
 CurveAnimator.prototype.stop = function(){
 	clearInterval(this.animTimer);
 };
+
 CurveAnimator.prototype.pointAt = function(percent){
 	return this.path.getPointAtLength(this.len*percent);
 };
+
 CurveAnimator.prototype.updatePath = function(){
 	this.len = this.path.getTotalLength();
 };
+
 CurveAnimator.prototype.setStart = function(x,y){
 	var M = this.path.pathSegList.getItem(0);
 	M.x = x; M.y = y;
 	this.updatePath();
 	return this;
 };
+
 CurveAnimator.prototype.setEnd = function(x,y){
 	var C = this.path.pathSegList.getItem(1);
 	C.x = x; C.y = y;
 	this.updatePath();
 	return this;
 };
+
 CurveAnimator.prototype.setStartDirection = function(x,y){
 	var C = this.path.pathSegList.getItem(1);
 	C.x1 = x; C.y1 = y;
 	this.updatePath();
 	return this;
 };
+
 CurveAnimator.prototype.setEndDirection = function(x,y){
 	var C = this.path.pathSegList.getItem(1);
 	C.x2 = x; C.y2 = y;
 	this.updatePath();
 	return this;
 };
+
+
+var animationCurve = [];
+
+requestAnimationFrame(function(){
+	
+	if(animationCurve){
+			
+	}
+});
--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-var svg   = document.querySelector('svg'),
-	path  = document.querySelector('path');
-var svgNS = svg.namespaceURI;
-var pt    = svg.createSVGPoint();
-var p1s   = path.pathSegList, m1 = p1s.getItem(0), c1 = p1s.getItem(1);
-
 var curve = new CurveAnimator(
   [50,300], [350,300],// position gauche/droite
   [50,100], [350,100] // croche gauche/droite
@@ -14,13 +8,3 @@ var o = document.getElementById('img');
 curve.animate(2, function(point,angle){
   o.style.webkitTransform = 'translateX('+point.x+'px) translateY('+point.y+'px) rotate('+angle+'deg)';
 });
-
-var lastCurve = CurveAnimator.lastCreated;
-var p2s = lastCurve.path.pathSegList, 
-	m2 = p2s.getItem(0), 
-	c2 = p2s.getItem(1);
-
-m1.x  = m2.x;  m1.y  = m2.y;
-c1.x  = c2.x;  c1.y  = c2.y;
-c1.x1 = c2.x1; c1.y1 = c2.y1;
-c1.x2 = c2.x2; c1.y2 = c2.y2;
